a23f35f9c89fbff31c39dd8a8b9a6e11676fd983
trenod <torewb@gmail.com>
2022-10-14 14:54:51 +0200

    Started coding the TypeScript and inserting into html via angular

--- todos/src/main/todo-app/src/app/app.component.ts
+++ todos/src/main/todo-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  constructor(private httpClient: HttpClient) {
+    this.loadTodos();
+  }
+
+  loadTodos(){
+    this.httpClient.get('http://localhost:8080/todos').subscribe((response) =>
+      alert(JSON.stringify(response)));
+
+  }
+
+  createTodo(id: number, summary: string, description: string) {
+    var toPost:String[];
+    toPost = [id.toString(), summary, description]
+    this.httpClient.post('http://localhost:8080/todos', toPost).subscribe((response) =>
+      alert(JSON.stringify(response)));
+  }
+
 }
